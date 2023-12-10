8923110517a1670be240b7b7ebd0137b83c816f1
Carlos Palma <carlos.palma.0491@gmail.com>
2023-12-10 00:23:07 -0500

    add layout default and import bootstrap css

---
+++ src/layouts/Default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="page-wrapper">
+        <slot />
+    </div>
+</template>
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default {
+        name: 'layoutDefault',
+    }
+</script>
