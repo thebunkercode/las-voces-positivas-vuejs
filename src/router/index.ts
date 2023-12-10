447f4623edf16ddd0062f4d177e2a17fd6ccb5bd
Carlos Palma <carlos.palma.0491@gmail.com>
2023-12-10 05:56:47 -0500

    agregando nuevas rutas de paginas y validador de session

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,9 @@ import LoginView from '../views/auth/LoginView.vue'
 import RegisterView from "@/views/auth/RegisterView.vue";
 import DashboardView from "@/views/DashboardView.vue";
 import PerfilView from "@/views/PerfilView.vue";
+import GroupsView from "@/views/GroupsView.vue";
+import ChallengesView from "@/views/ChallengesView.vue";
+import ScoreView from "@/views/ScoreView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,12 +29,32 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: DashboardView
+      component: DashboardView,
+      meta: { requiresAuth: true }
     },
     {
       path: '/perfil',
       name: 'perfil',
-      component: PerfilView
+      component: PerfilView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/grupos',
+      name: 'grupos',
+      component: GroupsView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/desafios',
+      name: 'challenges',
+      component: ChallengesView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/tablero-de-puntuacion',
+      name: 'score',
+      component: ScoreView,
+      meta: { requiresAuth: true }
     },
 
 
@@ -44,6 +67,24 @@ const router = createRouter({
     //   component: () => import('../views/AboutView.vue')
     // }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // Verifica si el usuario está autenticado (por ejemplo, si existe un valor en localStorage)
+    const userAuthenticated = localStorage.getItem('token');
+
+    if (!userAuthenticated) {
+      // Si no está autenticado, redirige al usuario a la página de inicio de sesión
+      next({ name: 'login' });
+    } else {
+      // Si está autenticado, permite la navegación
+      next();
+    }
+  } else {
+    // Si la ruta no requiere autenticación, permite la navegación
+    next();
+  }
+});
 
 export default router
