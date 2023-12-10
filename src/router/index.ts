e9c0cdd4836ced27f583dcfe68a09d1b028939df
Carlos Palma <carlos.palma.0491@gmail.com>
2023-12-10 00:27:16 -0500

    page register and store connect api

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/auth/LoginView.vue'
+import RegisterView from "@/views/auth/RegisterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/registrate',
+      name: 'register',
+      component: RegisterView
     },
 
     // {
--- src/store/auth.js
+++ src/store/auth.js
@@ -9,5 +9,14 @@ export default {
             .catch(error => ({ error: JSON.stringify(error) }));
 
         return response;
+    },
+    authRegister(user, email, password){
+        let response = axios.post(import.meta.env.VITE_API_URL + 'api/users/create', { 'user': user, 'email': email, 'password': password })
+            .then(response => {
+                return response.data;
+            })
+            .catch(error => ({ error: JSON.stringify(error) }));
+
+        return response;
     }
 };
