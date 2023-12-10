0d04786cc5878334684b869a4904a63d5cf07bb8
Carlos Palma <carlos.palma.0491@gmail.com>
2023-12-10 02:00:32 -0500

    pagina dashboard y su componente

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/auth/LoginView.vue'
 import RegisterView from "@/views/auth/RegisterView.vue";
+import DashboardView from "@/views/DashboardView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+      {
       path: '/login',
       name: 'login',
       component: LoginView
@@ -17,10 +23,11 @@ const router = createRouter({
       component: RegisterView
     },
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
     },
+
 
     // {
     //   path: '/about',
